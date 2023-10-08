facd70885daefc0283b67c015e95ee225728b98d
Joshua Noronha <noronhajoshua1@gmail.com>
2023-10-08 15:21:53 -0700

    Completed task5

--- Code/feature_models/feature_matrix/label_label_similarity.py
+++ Code/feature_models/feature_matrix/label_label_similarity.py
@@ -6,16 +6,18 @@ class LabelLabelSimilarity:
     def __init__(self, feature_vectors):
         self.feature_vectors = feature_vectors
         
-def get_matrix(feature_vectors):
-    centroids = find_centroids(feature_vectors)
-    label_list = centroids.keys()
-    result = np.zeros(shape=(len(label_list), (len(label_list))))
-    for i, item in enumerate(centroids.items()):
-        current_label, current_centroid = item
-        for j, label in enumerate(label_list):
-            # find similarity between current_centroid and centroids[label] and store in array
-            result[i][j] = max(1 - cosine(current_centroid, centroids[label]), 0)
-    return label_list, result
+    def get_matrix(self):
+        centroids = find_centroids(self.feature_vectors)
+        label_list = centroids.keys()
+        result = {}
+        for i, item in enumerate(centroids.items()):
+            current_label, current_centroid = item
+            arr = np.zeros(shape=(len(label_list)))
+            for j, label in enumerate(label_list):
+                # find similarity between current_centroid and centroids[label] and store in array
+                arr[j] = max(1 - cosine(current_centroid, centroids[label]), 0)
+            result[i] = (current_label, arr)
+        return result
 
 
         
