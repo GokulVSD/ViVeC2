40ad800319d5c8dd8f6b021aa91854195a5b7df9
Gokul Vasudeva <gokul.vasda@gmail.com>
2023-10-12 21:32:30 -0700

    Reorganized task 2b to follow the same pattern as task 2a

--- Code/utils/database_utils.py
+++ Code/utils/database_utils.py
@@ -18,16 +18,3 @@ def retrieve(filename):
     Retrieves .pt file in /Code/database/ as object.
     """
     return load(path.join(DATABASE_PATH, filename))
-
-
-def pt2df(FEATURE_SPACE):
-    data = retrieve(f'latent_{FEATURE_SPACE}.pt')
-    return pd.DataFrame(data).T
-
-# Reference: https://stackoverflow.com/questions/13331698/how-to-apply-a-function-to-two-columns-of-pandas-dataframe
-def combineRows(a, b):
-    return (a, b)
-
-def df2pt(df):
-    combineCol = df.apply(lambda x: test(x[0], x[1]), axis=1)
-    return combineCol.to_dict()
